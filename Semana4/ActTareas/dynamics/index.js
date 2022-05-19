68a431b136b7aad6a7e0f77aea03d74b7c33e90d
daniela-cardenas-b <daniela.cardenas@comunidad.unam.mx>
2022-05-19 18:57:56 -0500

    No me acordaba de una validación, ahora si :(

--- Semana4/ActTareas/dynamics/index.js
+++ Semana4/ActTareas/dynamics/index.js
@@ -9,12 +9,11 @@ var crearinput = document.createElement("input"); //texto de la materia nueva
 let pendientes=" tareas acabadas de ";
 let nombreMateria = materias.value, otraMat=0, child=0, totalTareas=0, completadas=0, conteo=0; //variables utilizadas al rededor las funciones iterativas 
 crearinput.innerHTML="<input class='texto' id='otra' type='text' placeholder='Nueva materia: '/>";
-
 // intenté agregar la materia al select :(
 // var = materiaselect = document.createElement("select"); 
 // materiaselect.innerHTML="<option values='"+nombreMateria + "'>"+nombreMateria + "</option>"; 
 //mi idea era dentro del evento en el que se verifica si se agrega otra materia o no--abajo 
-
+let nom = crearinput.innerHTML; 
 
 materias.addEventListener("change", (evento)=>
 {
@@ -34,33 +33,57 @@ materias.addEventListener("change", (evento)=>
     }
 });
 
+
 boton.addEventListener("click", (evento)=>{
     var tarea = input.value;
+    var noimprimir = 0; 
     if(input.value == "")
     {
             alert("Registra una tarea, por favor :) ");
     }
-    if(tarea != "")
-    {
-        if(otraMat != 1)
-            lista.innerHTML += "<div id='"+conteo+"' class='tarea'>"+"<div id='ariba'><span class='boton'><strong>"+nombreMateria+"</strong></span>"+'</div>'+'<br><br><div id="izquierda">'+tarea+'</div><div id="derecha"><button class="boton"id="completar">Terminado</button><br><button class="boton" id="borrar">Borrar</button>'+'</div></div>';
-        else
+    else{
+        if(tarea != "")
         {
-            nombreMateria=crearinput.value;
-            if(nombreMateria=="")
-                nombreMateria="Otra";
-            lista.innerHTML += "<div id='"+conteo+"' class='tarea'>"+"<div id='ariba'><span class='boton'><strong>"+nombreMateria+"</strong></span></div>"+'<br><br><div id="izquierda">'+tarea+'</div><div id="derecha"><button class="boton"id="completar">Terminado</button><br><button class="boton" id="borrar">Borrar</button>'+'</div></div>';
-        }
-        totalTareas++;
-        if(completadas==1)
-            pendientes=" tarea pendiente de "
-        else
-            pendientes=" tareas pendientes de "
-        marca.innerHTML=completadas+pendientes+totalTareas;
-        conteo++;
-    } 
+            if(otraMat != 1){
+                lista.innerHTML += "<div id='"+conteo+"' class='tarea'>"+"<div id='ariba'><span class='boton'><strong>"+nombreMateria+"</strong></span>"+'</div>'+'<br><br><div id="izquierda">'+tarea+'</div><div id="derecha"><button class="boton"id="completar">Terminado</button><br><button class="boton" id="borrar">Borrar</button>'+'</div></div>';
+                totalTareas++;
+                if(completadas==1)
+                    pendientes=" tarea pendiente de "
+                else
+                    pendientes=" tareas pendientes de "
+                marca.innerHTML=completadas+pendientes+totalTareas;
+                conteo++;
+            }
+            else
+            {
+                nombreMateria=crearinput.value;
+                if((nombreMateria == "Biología") || (nombreMateria == "Pintura") || ( nombreMateria == "Computación") || ( nombreMateria ==  "Geografía") || (nombreMateria ==  "Astronomía"))
+                {
+                    alert("Esta materia ya existe"); //si sirve está verificación, solo no funciona cuando se repite la tarea aaa
+                }
+                // console.log(nombreMateria.value); 
+                else
+                {
+                    nombreMateria="Otra";
+                    lista.innerHTML += "<div id='"+conteo+"' class='tarea'>"+"<div id='ariba'><span class='boton'><strong>"+nombreMateria+"</strong></span></div>"+'<br><br><div id="izquierda">'+tarea+'</div><div id="derecha"><button class="boton"id="completar">Terminado</button><br><button class="boton" id="borrar">Borrar</button>'+'</div></div>';    
+                    totalTareas++;
+                    if(completadas==1)
+                        pendientes=" tarea pendiente de "
+                    else
+                        pendientes=" tareas pendientes de "
+                    marca.innerHTML=completadas+pendientes+totalTareas;
+                    conteo++;
+                }
+            }
+        } 
+    
+    }
+    // console.log("Nueva"+crearinput.value); 
+    // console.log("Existe"+nombreMateria.value); 
+    // console.log("Materia"+materias.value); 
+    console.log(nom); 
 });
-
+    
 lista.addEventListener("click" , (evento)=>{
 
     if(evento.target.className== "boton")
